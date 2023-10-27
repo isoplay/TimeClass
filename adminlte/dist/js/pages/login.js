1baeb7d10ea77071ba037e7d89e27351bd7ae2f7
WESLEY BRUM <146049367+WESLEYBRUMM@users.noreply.github.com>
2023-10-27 22:07:03 +0000

    implementaçao da parte lembre-se de mim

--- adminlte/dist/js/pages/login.js
+++ adminlte/dist/js/pages/login.js
@@ -1,39 +1,104 @@
-function login(event) {
-    event.preventDefault(); // Isso evita a ação padrão do formulário (envio e atualização da página)
-  
-    const receberEmail = document.getElementById('login__email').value;
-    const receberSenha = document.getElementById('login__password').value;
-    
-    const url = 'https://api-agendamendo-jb.wesleybrum.repl.co/login';
-    const data = {
-      email: receberEmail,
-      senha: receberSenha
-    };
-  
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Erro na requisição');
-        }
-        return response.json();
-      })
-      .then(result => {
-        if (result == "usuario nao encontrado") {
-          console.log("Usuário não encontrado");
-        } else {
-          console.log("Login bem-sucedido", result);
-          window.location = "./adminlte/blank.html";
-        }
-      })
-      .catch(error => {
-        // Tratar erros de rede ou outras falhas
-        console.error(error);
-      });
+document.addEventListener('DOMContentLoaded', checkLocalStorageAndAuthenticate);
+
+function checkLocalStorageAndAuthenticate() {
+  const token = getFromLocalStorage('token');
+  const userId = getFromLocalStorage('userId');
+  const email = getFromLocalStorage('email');
+  const remember = getFromLocalStorage('lembre_se');
+
+  if (token && remember === 'true') {
+    autenticarUsuario(token, email, userId);
   }
-  
+}
+
+function getFromLocalStorage(key) {
+  return localStorage.getItem(key);
+}
+
+function setInLocalStorage(key, value) {
+  if (value) {
+    localStorage.setItem(key, value);
+  } else {
+    localStorage.removeItem(key);
+  }
+}
+
+function login(event) {
+  event.preventDefault();
+
+  const receberEmail = document.getElementById('login__email').value;
+  const receberSenha = document.getElementById('login__password').value;
+  const lembre_se = document.getElementById('remember');
+
+  const url = 'https://api-agendamendo-jb.wesleybrum.repl.co/login';
+  const data = {
+    email: receberEmail,
+    senha: receberSenha
+  };
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Erro na requisição');
+      }
+      return response.json();
+    })
+    .then(result => {
+      if (result === "usuario nao encontrado") {
+        console.log("Usuário não encontrado");
+      } else {
+        const token = result.token;
+        setInLocalStorage('token', token);
+        setInLocalStorage('email', result.payload.email);
+        setInLocalStorage('userId', result.payload.userId);
+
+        if (lembre_se.checked) {
+          setInLocalStorage('lembre_se', 'true');
+        } else {
+          setInLocalStorage('lembre_se', null);
+        }
+
+        autenticarUsuario(token, result.payload.email, result.payload.userId);
+      }
+    })
+    .catch(handleError);
+}
+
+function autenticarUsuario(token, email, userId) {
+  const url = 'https://api-agendamendo-jb.wesleybrum.repl.co/auth-auto';
+
+  const data = {
+    email,
+    userId
+  };
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': token
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => {
+      if (!response.ok) {
+        console.log('Erro na requisição');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log("Autenticação bem-sucedida", data);
+      window.location = "./adminlte/blank.html";
+    })
+    .catch(handleError);
+}
+
+function handleError(error) {
+  console.error(error);
+}
